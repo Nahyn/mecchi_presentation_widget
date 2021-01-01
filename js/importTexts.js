0a01929226a516354e67895213ca11a6a1951844
Nahyn <benj33760@hotmail.fr>
2021-01-01 13:41:15 +0100

    Add files via upload

--- js/importTexts.js
+++ js/importTexts.js
@@ -1,5 +1,4 @@
 var importedTexts = {
-	
 	"characters" : {
 		"stray": {
 			"name": "[UNKNOWN] [UNKNOWN]",
@@ -10,7 +9,7 @@ var importedTexts = {
 			"description": [
 				"Born from an unknown family. He is unusually small and scrawny for a demon, chances of surviving as a kid were low. Seems like he doesn't know, or perhaps doesn't want to talk. Possibly has selective mutism. Lack of eyebrows and missing teeth (his canine seems to still be present, though visibly dull). His diet consists principally of meat and fish. Any other kind of food was observed to disrupt his digestive system. Nonetheless, his favorite food seems to be strawberries.",
 				"Traces of his traumas as a teenager have carried on to his adult years. He doesn't express his needs, desires or opinions. He principally spends his time observing his surrounding, looking for facial cues. He appears to have no visible ambitions. By contrast, he appears to be adamant about acting according to his intuition. He hasn't been observed to laugh or smile very often. Has never been observed to be mad. ",
-				"He seems to be living with a human nicknamed \"Butcher\", in his apartment. Their meeting is believed to have happened the day Stray fell unconscious in front of the human's house [exact day needed]. They seem to have not known each other before this incident. Past pictures suggest that Stray was undernourished and in a state of hypothermia at the time of his meeting with Butcher. No informations are currently available about their relationship. Another human he is in contact with is Galileo. He seems to be considering her like a close sibling and is the only known person he is affectionate with. No info is available explaining this behavior. These two humans are the only people he is in contact with.",
+				"He seems to be living with a human nicknamed Butcher, in his apartment. Their meeting is believed to have happened the day Stray fell unconscious in front of the human's house [exact day needed]. They seem to have not known each other before this incident. Past pictures suggest that Stray was undernourished and in a state of hypothermia at the time of his meeting with Butcher. No informations are currently available about their relationship. Another human he is in contact with is Galileo. He seems to be considering her like a close sibling and is the only known person he is affectionate with. No info is available explaining this behavior. These two humans are the only people he is in contact with.",
 			],
 			
 			"timePeriods" : {
@@ -59,7 +58,7 @@ var importedTexts = {
 			"description": [
 				"A human with muscular mass and height slightly above average. Naturally wavy dark brown hair and dark brown eyes. Works as a freelancing butcher in his own shop. Income seems to be low. Does not have a partner nor children. Sources point towards a dislike of products made with milk [lactose intolerance was not found], but a liking to spicy dishes, which implies he simply does not have any good taste in food. Suffer from slight bad vision.",
 				"He is often seen being friendly towards other people and has a tendency to help strangers: one instance of him saving a scared kitten from a tree was observed. This should be noted as ESB. He is registered as a member of the gym situated in the Corkscrew street and goes 4 times a week. Evidences show a vital need to take care for his appearence. Said appearance seems to add to the popularity to his shop [correlation study needed]. ",
-				"The meeting with the demon \"Stray\" happened during the winter of year XXXX [exact day needed]. Butcher found the unconscious body of \"Stray\" in the snow in front of his shop. He carried him inside his apartment and called \"Galiléo\" to the rescue. They did not call the hospital [speculated reason: lack of funds] and gave him first aid. \"Galiléo\" was seen going back to work three hours later. It is not known when exactly \"Stray\" gained his consciousness back, but Butcher did not open his shop that day. Doesn't have contact with any other demon. Situation seems stable. ",
+				"The meeting with the demon Stray happened during the winter of year XXXX [exact day needed]. Butcher found the unconscious body of Stray in the snow in front of his shop. He carried him inside his apartment and called Galiléo to the rescue. They did not call the hospital [speculated reason: lack of funds] and gave him first aid. Galiléo was seen going back to work three hours later. It is not known when exactly Stray gained his consciousness back, but Butcher did not open his shop that day. Doesn't have contact with any other demon. Situation seems stable. ",
 			],
 			
 			"timePeriods" : {
@@ -69,12 +68,12 @@ var importedTexts = {
 
 				"teen": [
 					"Attention from his nanny and sister faded as he became a teenager. It seems to correlate with the period when he started his rebellious phase. Rebellious phase started at age 15. First cigarette smoked at age 16. Sources show that his friends were what influenced his behavior and actions. Was seen shoplifting two times and cheating on his tests eleven times. His parents divorced at age 16 and he started living alone with his mom while his sister went abroad.",
-					"It's apparent that his relationship with his mom stayed difficult thorough his teenager years. One of his diary page included the mention of a \"movie night\" ritual every week-end, where they would watch badly rated romance movies and cry. He seems to imply that those \"movie nights\" were the only time he felt close to his mom. ",
+					"It's apparent that his relationship with his mom stayed difficult thorough his teenager years. One of his diary page included the mention of a movie night ritual every week-end, where they would watch badly rated romance movies and cry. He seems to imply that those movie nights were the only time he felt close to his mom. ",
 				],
 
 				"present": [
-					"He changed drastically when he started his butchery studies (age 19). Changes includes: stopping smoking, building muscular mass, changing social circle and stopping any contact with his parents altogether. He met another human named \"Galiléo\" during this period and became close friend with her. He opened a butcher shop at age 21 in the Shovel Warehouse Street. Met the demon \"Stray\" six months later.",
-					"He has developped signs of attachment towards \"Stray\" since their meeting, though the extends of those feelings have not been studied yet. ",
+					"He changed drastically when he started his butchery studies (age 19). Changes includes: stopping smoking, building muscular mass, changing social circle and stopping any contact with his parents altogether. He met another human named Galiléo during this period and became close friend with her. He opened a butcher shop at age 21 in the Shovel Warehouse Street. Met the demon Stray six months later.",
+					"He has developped signs of attachment towards Stray since their meeting, though the extends of those feelings have not been studied yet. ",
 				],
 
 				"future": [
@@ -84,44 +83,53 @@ var importedTexts = {
 			},
 			
 			"gallery" : {
+				"image_00" : "Bad vision.",
+				"image_01" : "Daydreaming.",
+				"image_02" : "New Year.",
+				"image_03" : "Streetwear fashion.",
+				"image_04" : "Sleeping beauty.",
+				"image_05" : "Dressing up as Aikawa and Risu (Dorohedoro). ",
+				"image_06" : "Laundry accident.",
 			}
 		},
 		
 		"galiléo": {
-			"name": "[redacted] [redacted]",
-			"age": "XXyo",
-			"date of birth": "XX/XX/XXXX",
-			"specie": "[long]",
+			"name": "GALILEO [UNKNOWN]",
+			"age": "22",
+			"date of birth": "22 JUN XX",
+			"specie": "human",
 			
 			"description": [
-				"Lorem ipsum dolor sits amet, consectetur adipiscing elit. Phasellus feugiat sollicitudin luctus. Integer ut mauris gravida, fringilla velit quis, commodo leo. Vivamus eget risus justo. Aliquam tellus sapien, ultrices tincidunt tellus non, sodales euismod augue. Suspendisse volutpat efficitur quam, eu ultricies neque venenatis ut. Proin risus arcu, bibendum nec hendrerit quis, finibus at tortor. Proin finibus vel magna in lacinia. Suspendisse sapien mauris, maximus in erat non, aliquet consectetur ligula. Fusce pretium lectus ac consectetur pulvinar. Vivamus ac consequat tortor, quis mattis velit. Aliquam scelerisque lacinia nulla ut tincidunt. Aliquam erat volutpat. Nam lacinia pulvinar justo sit amet convallis. Nulla tempor massa et turpis sodales, efficitur placerat velit euismod. Mauris posuere dapibus quam, sed dapibus dolor pulvinar in. Vestibulum feugiat imperdiet lorem, vitae gravida justo congue eu.",
-				"Aliquam non leo vel leo egestas egestas rhoncus ut purus. Etiam velit odio, congue id varius eget, suscipit ut magna. Praesent congue mauris vitae antes stellus auctor, vitae interdum leo volutpat. Proin viverra nisl velit. Maecenas ut venenatis tortor, a convallis ex. Nam dapibus neque eu est pharetra cursus. Maecenas iaculis nulla lacus, et malesuada enim imperdiet non. In pulvinar, sapien in pellentesque maximus, risus ante fermentum sem, sed gravida diam magna et libero. Etiam sed sodales enim.",
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ex mauris, tincidunt sits amet porttitor ut, pharetra sed lacus. Aliquam erat volutpat. Nunc facilisis odio tellus, ut tincidunt turpis posuere quis. Nulla sit amet nunc lectus. Sed et ligula vestibulum, laoreet quam eget, laoreet elit. Quisque quam nisi, posuere vel iaculis in, faucibus ut leo. Morbi egestas magna leo, cursus pharetra quam maximus in. Donec auctor eros quis mi posuere, ut feugiat sem ultricies. Proin vitae auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+				"A human of average stature and size. Works as a construction worker and does not seem to have any hobby or interests. She seems to be extremely secretive about her life and past, making it hard to collect informations. The only two people she appears to be in contact with are the demon Stray and the human Butcher. She has a very definite routine and rarely goes out of her way to change it: this could be an habit taken from her childhood [more infos needed].",
+				"It is hard at this point in time to correctly describe her personality, as her most often face expressions are reduced to a polite smile and a neutral expression. She has also been observed to be extremely cautious of her surroundings. Neither her family nor colleagues have additional informations as the rare personality trait she has showed through the entire time of research was an aloof behavior with most of her entourage. The only two notable exceptions to this are the forementionned demon Stray and human Butcher.",
+				"She seems to be experiencing a normal friendship with Butcher, while still keeping a wall between them [not enough informations are available to determine if this behavior is done on purpose or not]. An extremely important relationship she has is the one with Stray: the moments with him are the only few where she's seen to be behave in a more genuine way. Their relationship could be described as one of a bigger sister and little brother, as she was seen being protective over him on multiple occasion. This will most probably become a valuable source of informations.",
 			],
 			
 			"timePeriods" : {
 				"baby": [
-					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ex mauris, tincidunt sit amet porttitor ut, pharetra sed lacus. Aliquam erat volutpat. Nunc facilisis odio tellus, ut tincidunt turpis posuere quis. Nulla sit amet nunc lectus. Sed et ligula vestibulum, laoreet quam eget, laoreet elit. Quisque quam nisi, posuere vel iaculis in, faucibus ut leo. Morbi egestas magna leo, cursus pharetra quam maximus in. Donec auctor eros quis mi posuere, ut feugiat sem ultricies. Proin vitae auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-					"Quisque vestibulum nulla vel mauris hendrerit, ut facilisis nulla sodales. Suspendisse potenti. Phasellus rutrum nisl est, in laoreet turpis porttitor in. Sed sed tortor et quam pellentesque congue quis vitae turpis. Aliquam erat volutpat. In at lacinia nunc, et accumsan leo. Ut at velit at leo tincidunt tristique. In hac habitasse platea dictumst.",
+					"No informations about her early childhood is available to this day. This may be caused by a secluded life, with not contact to the outside world, or a deliberate attempt made by her or someone else to hide her past.",
 				],
 
 				"teen": [
-					"Aliquam non leo vel leo egestas egestas rhoncus ut purus. Etiam velit odio, congue id varius eget, suscipit ut magna. Praesent congue mauris vitae tellus auctor, vitae interdum leo volutpat. Proin viverra nisl velit. Maecenas ut venenatis tortor, a convallis ex. Nam dapibus neque eu est pharetra cursus. Maecenas iaculis nulla lacus, et malesuada enim imperdiet non. In pulvinar, sapien in pellentesque maximus, risus ante fermentum sem, sed gravida diam magna et libero. Etiam sed sodales enim.",
-					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ex mauris, tincidunt sit amet porttitor ut, pharetra sed lacus. Aliquam erat volutpat. Quisque vestibulum nulla vel mauris hendrerit, ut facilisis nulla sodales. Suspendisse potenti. Phasellus rutrum nisl est, in laoreet turpis porttitor in. Sed sed tortor et quam pellentesque congue quis vitae turpis. Aliquam erat volutpat. In at lacinia nunc, et accumsan leo. Ut at velit at leo tincidunt tristique. In hac habitasse platea dictumst.",
+					"Just like her early childhood, not much is known about her teen years. The first record of a public appearence was at the age of 14, without any sign of biological parents. She was put in an orphanage, then adopted by a family. She seems to have moved out and started living alone as soon as she turned 18. She met Butcher when she was 19, during one of his internships.  ",
+					"Her behavior since her first sighting at 14 has always been very cold and secretive. Lots of informations are missing, no matter how extensive the researches are. This could be a sign of ESB. It is possible she will start to reveal more about her past the more she gets in contact with other people. ",
 				],
-				
+
 				"present": [
-					"Morbi ullamcorper dui in odio facilisis aliquet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean id ultrices quam. Duis blandit justo vel euismod rutrum. Curabitur neque diam, posuere consectetur purus at, convallis congue erat. Quisque gravida maximus fringilla. Vivamus id odio dui. Donec tincidunt nulla a consectetur auctor. Donec malesuada mauris velit, at egestas felis accumsan in. Mauris tristique neque ac viverra suscipit. Nunc vitae ultrices ligula. Donec aliquet scelerisque dui, id euismod diam egestas quis.",
-					"Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur id euismod velit. Ut placerat sem enim, sit amet aliquet turpis sagittis dignissim. Phasellus mattis efficitur neque sed venenatis. Pellentesque ac tempus tortor, vitae condimentum lectus. Quisque lobortis ut quam sed tincidunt. Ut eleifend orci non elit laoreet, dictum rhoncus elit mattis. Mauris eget turpis sit amet nunc consequat congue. Duis tincidunt magna nunc, a sodales eros pretium nec. In pharetra metus in iaculis tincidunt. Maecenas non bibendum risus, vitae consectetur libero. Ut vulputate dictum nisl, at dictum mauris luctus et. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+					"She has been working as a construction worker for 2 years, but socializing with her coworkers seem to be near to inexistant.  She still spends most of her time alone inside her apartment, rarely going outside with the exception of visiting Butcher and grocery shopping. Though she doesn't seem to use postal delivery, there has been instances of her meeting other people to buy such things as furnitures or small home appliance. It is also worth noting that she has never let anyone inside her home, including Butcher.",
+					"Though it appears that she has has opened up more than during her teenager years, there is still a lot of informations being hid away. She might have realized something. It's crucial to stay discreet and keep studying her behavior.",
 				],
-				
+
 				"future": [
-					"Integer fringilla odio tempor tempor porttitor. Aenean ut ultricies ante. Duis eu risus eu diam pellentesque varius. Donec sodales blandit urna, ac pretium magna. Quisque porttitor velit neque, id iaculis ante ultrices sed. Praesent interdum sem justo, vel aliquam nibh tincidunt non. Sed finibus imperdiet arcu, sit amet dignissim felis viverra sit amet. Vestibulum consectetur pulvinar dolor, a facilisis sem rutrum non. Fusce ac nunc pretium, eleifend ligula quis, blandit mi.",
-					"Fusce ullamcorper risus ut mi aliquam, efficitur pellentesque ligula vehicula. Mauris neque erat, bibendum at scelerisque id, varius vel ante. Nullam molestie posuere accumsan. Suspendisse auctor imperdiet faucibus. Integer quis vestibulum mauris, eget vulputate sapien. Sed mollis auctor velit at molestie. Aenean eget neque est. Aenean imperdiet eget nunc eu vestibulum. Aliquam imperdiet ante velit, ut ultrices nulla scelerisque ac. Sed tristique ex ut neque fringilla rhoncus. Sed sed orci consectetur, fermentum ipsum nec, ullamcorper felis. Sed a quam sollicitudin, consequat turpis in, maximus ligula. In semper urna at tellus consequat ornare. Vestibulum tristique consequat ultricies. Nulla a mattis nisl, at eleifend urna. Aenean eu magna vel elit bibendum bibendum at a felis.",
+					"More informations needed.",
+					"[REDACTED]",
 				],
 			},
 			
 			"gallery" : {
+				"image_00" : "Thunderstorm.",
+				"image_01" : "Past Uniform.",
+				"image_02" : "Day out.",
 			},
 		},
 	}
